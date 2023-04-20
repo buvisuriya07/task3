d39b0cfbbf74f6ab48dfac05bef54532ce4f65c4
buvisuriya07 <70657998+buvisuriya07@users.noreply.github.com>
2023-04-21 00:02:36 +0530

    Add files via upload

---
+++ resume using JSON.js
@@ -0,0 +1,66 @@
+{
+    "basics"; {
+      "name"; "Buvisuriya",
+      "email"; "buvisuriya29@gmail.com",
+      "phone"; "6379172734.",
+      "degree"; "B.E(Computer science)",
+      "summary"; "To succeed in an environment of growth and excellence and earn a job which provides me jobsatisfaction and self-development and help me achieve personal as well as organisational goals.",
+      "location"; {
+        "address";"117/42A,MittaOffice Street,Krishnapuram."
+        "postalCode"; "627751",
+        "city"; "kadayanallur",
+        "countryCode";"India",
+      "profiles"; [
+        {
+          "network": "LinkedIn",
+          "username": "buvisuriya",
+          "url": "www.linkedin.com/in/buvi-suriya1536772140",
+        }
+      ]
+    }
+   
+    
+    "education";[
+      {
+        "institution": "Velammal college of engineering and technology",
+        "area": "Computer science",
+        "studyType": "Full time",
+        "startDate": "2019-06-01",
+        "endDate": "2023-05-31",
+        "gpa": "8.0",
+      }
+    ],
+   
+    "publications"; [
+      {
+        "name": " Automatic Question Paper Generator Using Shuffling Algorithm",
+        "publisher": "Buvisuriya",
+        "releaseDate": "2022-06-24",
+        "website": "http://ijircce.com/admin/main/storage/app/pdf/IqORJRwmGmoNw6qniELffS tcRdRMVXLXGUlgUOQ.pdf",   
+      }
+    ],
+    "skills"; [
+      {
+        "name":  'Programming Languages',
+        "level": "intermediate",
+        "keywords": [
+          "( 'Java', 'C++', 'html','css','javascript'.)"
+        ]
+      }
+    ],
+    "languages"; [
+      {
+        "language": "tamil,english",
+        "fluency": "tamil"
+      }
+    ],
+    "interests";[
+      {
+        "name": 'Sports',
+        "keywords": [
+          " ( 'Cricket', 'Football')"
+        ]
+      }
+    ]
+  }
+}
